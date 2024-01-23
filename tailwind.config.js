793c3fb10e5360198dba312c0b1635dc27bdc4ba
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2024-01-23 08:05:07 +0600

    dark mode initialized and done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#1E293B",
+      },
+    },
   },
   plugins: [],
 };
