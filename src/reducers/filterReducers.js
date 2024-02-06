804c0ab8da6abfdcc123dd670c45d7ab67cb50f9
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2024-02-06 11:11:26 +0600

    clear filter done

--- src/reducers/filterReducers.js
+++ src/reducers/filterReducers.js
@@ -18,7 +18,13 @@ export const filterReducer = (state, action) => {
       return { ...state, onlyInStock: payload.onlyInStock };
 
     case "CLEAR_FILTER":
-      return;
+      return {
+        ...state,
+        onlyInStock: false,
+        bestSellerOnly: false,
+        sortBy: null,
+        ratings: null,
+      };
 
     default:
       throw new Error("No Cae Found!");
