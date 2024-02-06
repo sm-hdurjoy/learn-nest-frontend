0de458f42df3eba479bdf5d53f201f8e08c55214
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2024-02-06 08:01:09 +0600

    search functionality done

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import { useEffect, useState } from "react";
+import { Search } from "../Sections/Search";
 
 export const Header = () => {
   const [darkMode, setDarkMode] = useState(
     JSON.parse(localStorage.getItem("darkMode")) || false
   );
 
+  const [searchSection, setSetsearchSection] = useState(false);
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
 
@@ -32,7 +34,10 @@ export const Header = () => {
               className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"
               onClick={() => setDarkMode(!darkMode)}
             ></span>
-            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"></span>
+            <span
+              className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"
+              onClick={() => setSetsearchSection(!searchSection)}
+            ></span>
             <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
               <span className="text-2xl bi bi-cart-fill relative">
                 <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">
@@ -44,6 +49,7 @@ export const Header = () => {
           </div>
         </div>
       </nav>
+      {searchSection && <Search />}
     </header>
   );
 };
